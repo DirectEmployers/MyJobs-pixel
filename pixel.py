8fa333b89c4fcbc83eb1770aee9796398da06e18
Rick Wehrle <rick@directemployersfoundation.org>
2014-02-15 22:23:03 -0500

    Change check cookie to update since goal is to extend expiration

--- pixel.py
+++ pixel.py
@@ -7,7 +7,8 @@ from flask import Flask, make_response, request, redirect
 app = Flask(__name__)
 app.permanent_session_lifetime = timedelta(days=365)
 
-def get_cookie_domain(host):
+
+def get_cookie_domain():
     """
     Determines the domain for a domain-wide cookie
 
@@ -22,7 +23,7 @@ def get_cookie_domain(host):
     :host: Trimmed version of input :host: suitable as a domain-wide cookie
     """
     # Strip out port number, if any (likely on localhost)
-    host = host.split(':')[0]
+    host = request.host.split(':')[0]
 
     # According to assumptions, these must be domain and tld
     host = host.split('.')[-2:]
@@ -30,27 +31,45 @@ def get_cookie_domain(host):
     return '.' + '.'.join(host)
 
 
-def check_or_set_cookie(response):
+def update_or_set_cookie(response):
     """
-    Determines if there is an aguid cookie and P3P policy associated
-    with the current request
+    Determines if there is an aguid cookie with the current request
 
-    Updates cookie expiration if it already exists and is valid
     Updates cookie value if it is not valid
-    Sets a domain-wide cookie if there isn't one
+    Updates cookie expiration if it already exists and is valid
+    Sets a domain-wide aguid cookie if there is not one
+    Adds P3P Policy
     """
-    try:
-        aguid = uuid.UUID(request.cookies.get('aguid'))
-    except:
-        aguid = uuid.uuid4()
-
-    # Update cookie expiration
-    domain = get_cookie_domain(request.host)
-    expires = datetime.utcnow() + app.permanent_session_lifetime
-    response.set_cookie('aguid', str(aguid.hex),
-                        expires=expires,
-                        domain=domain)
+    # Add P3P Policy
     response.headers['P3P'] = 'CP="ALL DSP COR CURa IND PHY UNR"'
+
+    expires = datetime.utcnow() + app.permanent_session_lifetime
+    domain = get_cookie_domain()
+
+    try:
+        # Validate aguid cookie
+        aguid = uuid.UUID(request.cookies.get('aguid'))
+    except (ValueError, TypeError):
+        # Set new aguid value
+        aguid = uuid.uuid4()
+    finally:
+        # Update expiration or set aguid cookie
+        response.set_cookie('aguid', aguid.hex,
+                            expires=expires, domain=domain)
+
+    try:
+        # Validate myguid cookie
+        myguid = uuid.UUID(request.cookies.get('myguid'))
+    except ValueError:
+        # Delete invalid myguid cookie
+        response.set_cookie('myguid', '', expires=0, domain=domain)
+    except TypeError:
+        # Nothing to do since myguid does not exist
+        pass
+    else:
+        # Update myguid cookie expiration
+        response.set_cookie('myguid', myguid.hex,
+                            expires=expires, domain=domain)
 
 
 @app.route("/pixel.gif")
@@ -60,7 +79,7 @@ def pixel_gif():
     """
     response = make_response(PIXEL)
     response.headers['Content-Type'] = 'image/gif'
-    check_or_set_cookie(response)
+    update_or_set_cookie(response)
     return response
 
 
@@ -71,7 +90,7 @@ def favicon_ico():
     """
     response = make_response(FAVICON)
     response.headers['Content-Type'] = 'image/x-icon'
-    check_or_set_cookie(response)
+    update_or_set_cookie(response)
     return response
 
 
